2f373f7947f3216fa3b7719d52b8863478f33c62
paulforrs <paulforros@gmail.com>
2023-04-12 23:02:42 +0800

    adds next function to history feature

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ const WINNINGCOMBO = [
     [0,3,6],[1,4,7],[2,5,8],
     [0,4,8],[6,4,2]
 ]
+const history = {
+    moves : [],
+    moveCounter : 0,
+    recordMove : function(index, currentClass){
+        // this.moveCounter = this.moveCounter +1;
+        this.moves.push({[index]: currentClass})
+        console.log(history.moves);
+    }
+}
 // Pages
 const homeMenu = document.querySelector('.home-menu')
 const gameContainer = document.querySelector('.game-container')
@@ -47,32 +56,78 @@ homeButton.forEach(button =>{
         }
     })
 })
+// Replay
+let turnCounter = 0;
+function replayBack(){
+
+}
+function replayNext(){
+
+}
+function handleReplay(e){
+    id = e.target.id
+    const moveIndex = history.moves[turnCounter]
+    if(id === 'replay-back'){
+        console.log(history.moves[turnCounter])
+        // back
+    }
+    else if( id ==='replay-next'){
+
+        for( var cellIndex in moveIndex){
+            const cell = document.querySelector(`[data-cell= "${cellIndex}"]`)
+            console.log(cell,moveIndex[cellIndex])
+            cell.classList.add( moveIndex[cellIndex])
+        }
+        turnCounter++
+    }
+    else{
+        // exit
+    }
+}
+function replay(){
+    console.log('replay function');
+    const replayButtons = Array.from(document.querySelectorAll('.replay-button'))
+    replayButtons.forEach( (button) =>{
+        console.log(button)
+        button.addEventListener('click',
+            handleReplay
+        )
+    })
+}
 
 // Winner Message
 const endGameButtons = Array.from(document.querySelectorAll('.end-game-button'))
 console.log(endGameButtons)
 endGameButtons.forEach(button=>{
     button.addEventListener('click', (e)=>{
-        if(e.target.id == 'end-home'){
+        if(e.target.id === 'end-home'){
             hideMessage()
             hideGame()
             showHome()
-            resetGame()
+            resetBoard()
+        }
+        else if(e.target.id === 'end-replay'){
+            console.log('replay')
+            resetBoard()
+            hideMessage()
+            replay()
         }
         else{
             hideMessage()
-            resetGame()
+            resetBoard()
+            startUp()
         }
     })
 })
 
-function resetGame(){
+function resetBoard(){
+    board.classList.remove(X_CLASS)
+    board.classList.remove(O_CLASS)
     cells.forEach(cell=>{
         cell.classList.remove('x')
         cell.classList.remove('o')
         cell.removeEventListener('click', handleClick)
     })
-    startUp()
 }
 
 // Board Cells
@@ -87,7 +142,9 @@ function addClickEvent(){
 function handleClick(e){
     const cell = e.target
     const currentClass = xTurn == true ? X_CLASS : O_CLASS;
+    const cellIndex = e.target.getAttribute('data-cell')
     addMarker(cell)
+    history.recordMove(cellIndex, currentClass)
     if(checkWinner(currentClass)){
         const winner = `${currentClass.toUpperCase()} wins !`
         endGame(winner)
